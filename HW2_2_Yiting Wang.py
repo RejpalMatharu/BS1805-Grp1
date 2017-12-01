96a13519c60545d4315296b61c5ce81a3d79cc4b
yiting-wang <32194559+yiting-wang@users.noreply.github.com>
2017-12-01 19:55:11 +0000

    Programming Question 2a and 2b - Yiting Wang
    2a: distance matrix; scatter plot
    2b: TSP best route

--- HW2_2_Yiting Wang.py
+++ HW2_2_Yiting Wang.py
@@ -4,6 +4,7 @@ Created on Thu Nov 30 00:36:12 2017
 
 @author: Yiting
 """
+
 # Question 2a
 import math
 import pandas as pd
@@ -11,7 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import combinations
 
-#read file
+# read file
 fhand = open('C:/MSBA17-18/4. Network Analytics/HW2/HW2_tsp.txt', 'r')
 
 file = list()
@@ -51,8 +52,42 @@ for a, b in combinations(range(38),2):
 
 print(dist_matrix) 
 
-# plot the latitude and longitude as a scatter plot
+# plot the latitude and longitude as a scatter plot (credit to Letty...)
 lons = data['longitude'].tolist()
 lats = data['latitude'].tolist()
 plt.figure(figsize=(18,18))
 plt.scatter(lons,lats)
+
+
+# Question 2b
+# reference from https://github.com/google/or-tools/blob/master/examples/python/tsp.py
+from ortools.constraint_solver import pywrapcp
+from ortools.constraint_solver import routing_enums_pb2
+
+def Distance(i, j):
+    return dist_matrix[i][j]
+
+routing = pywrapcp.RoutingModel(38, 1, 0)
+search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
+search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+routing.SetArcCostEvaluatorOfAllVehicles(Distance)
+
+assignment = routing.Solve()
+
+if assignment:
+    # Solution cost.
+    print(assignment.ObjectiveValue())
+    # Inspect solution.
+    # Only one route here; otherwise iterate from 0 to routing.vehicles() - 1
+    route_number = 0
+    node = routing.Start(route_number)
+    route = ''
+    while not routing.IsEnd(node):
+        route += 'city ' + str(node + 1) + ' -> '
+        node = assignment.Value(routing.NextVar(node))
+    route += 'city 1'
+    print(route)
+else:
+    print('No solution found.')
