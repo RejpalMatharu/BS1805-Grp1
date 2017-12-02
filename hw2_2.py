d5618a29aa2a32d132bde7550d356083ad0db642
thamster1 <32263801+thamster1@users.noreply.github.com>
2017-12-02 16:06:47 +0000

    Added B
    Edits in Part A:
    Re-indexed distance matrix/dataframe to make it convertible into NetworkX Graph.
    Created a positional dictionary for Graph position plot (will be used in Part C)
     **I regraphed part A as a NetworkX graph, just since I'd be using everything required for it later as well anyway. We can change it back if necessary

--- hw2_2.py
+++ hw2_2.py
@@ -1,16 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec  1 12:06:50 2017
-
-@author: letty
-"""
-
 import pandas as pd
-import geopy
-from geopy.distance import great_circle
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
-
+import gurobipy as grb
+import networkx as nx
+from geopy.distance import vincenty
 
 #read, adjust data
 file = open('HW2_tsp.txt', 'r')
@@ -23,32 +14,112 @@ data["city"] = pd.to_numeric(data["city"])
 data["latitude"] = pd.to_numeric(data["latitude"])/1000
 data["longitude"] = pd.to_numeric(data["longitude"])/1000
 data['position'] = data['latitude'].astype(str) +','+ data['longitude'].astype(str)
-#data['position'] = data[data.columns[1:]].apply(lambda x: ','.join(x.dropna()),axis=1)
-
-
-
 
 #calculate great circle distance of each pair of the city
-#i did not divide it by 1000 in this calculation tho***
 distance=list()
 for i in range(38):
     for j in range(38):
-        distance.append((great_circle(data.loc[i,'position'],data.loc[j,'position']).km))
+        distance.append((vincenty(data.loc[i,'position'],data.loc[j,'position']).km))
 
 chunks = [distance[x:x+38] for x in range(0, len(distance), 38)]
 
 #turn into a distance matrix
-city=list()
-for i in range(1,39):
-    city.append('city '+str(i))
-matrix=pd.DataFrame(chunks,columns=city)
+cities = []
+for i in range(1, len(data)+1):
+    cities.append('city ' + str(i))
+distances = pd.DataFrame(chunks, columns=cities, index=cities)
 
 #Question a
 
-lons = data['longitude'].tolist()
-lats = data['latitude'].tolist()
+#Create X,Y coordinates of each node
+xypos = {}
+for i in range(len(data)):
+    xypos['city ' + str(data['city'][i])] = ( data['longitude'][i] , data['latitude'][i])
 
-plt.figure(figsize=(18,18))
-plt.scatter(lons,lats)
+G = nx.from_pandas_adjacency(distances)
+nx.draw_networkx_nodes(G, pos = xypos)
+nx.draw_networkx_labels(G, pos = xypos)
 
-#Question b/c
+
+#Question c
+#Definining several functions to use later in a while loop
+def saveresult(draw=True):
+    """
+    To be used after optimizing a Gurobi model.
+    Saves the result of a Gurobi Optimizer solution as a Graph, returning NetworkX Graph Object.
+    Use draw=True argument to draw the graph.
+    """
+
+    vals = m.getAttr('x', vars)
+    res=[]
+    for i,j in vals.items():
+        if j>0:
+            res.append(i)
+    FG = nx.Graph()
+    FG.add_nodes_from(G)
+    FG.add_edges_from(res)
+    if draw == True:
+        nx.draw_networkx(FG, pos=xypos)
+    return (FG)
+
+def findsubtour(currentGraph, startnode):
+    """
+    From a NetworkX Graph, uses BFS to find all nodes linked to a starting node.
+    Returns a set that has all nodes which are in the same subtour as the starting node. 
+    """
+    mainsubtour = set()
+    for k,v in dict(nx.bfs_successors(currentGraph, startnode)).items():
+        if k not in mainsubtour:
+            mainsubtour.add(k)
+        for i in v:
+            if i not in mainsubtour:
+                mainsubtour.add(i)
+    return (mainsubtour)
+
+def addcuttomodel(fullgraph, subtourset, Model):
+    """
+    Identifies the cut (list of edges) that creates the subtour in subtourset.
+    Adds a constraint to a Gurobi model specifying that the sum of this subtour >= 2. 
+    """
+    #Define the cut separating this subtour
+    cut = []
+    for i,j in fullgraph.edges():
+        if i in subtourset and j not in subtourset:
+            cut.append((i,j))
+        elif j in subtourset and i not in subtourset:
+            cut.append((j,i))
+    
+    #Add cut constraint
+    varsx = Model.addVars(cut)
+    for i,j in varsx.keys():
+        varsx[i,j] = vars[i,j]
+    Model.addConstr(varsx.sum() >= 2)
+    
+
+#Generate Gurobi model
+m = grb.Model()
+
+#Create variables - edges
+edgedict = dict(nx.get_edge_attributes(G, 'weight'))
+vars = m.addVars(edgedict.keys(), obj=edgedict, vtype=grb.GRB.BINARY, name='edge')
+for i,j in vars.keys():
+    vars[j,i] = vars[i,j] # edge in opposite direction is equal and referred to edge
+
+#Add initial constraints: Degree of each node = 2
+m.addConstrs(vars.sum(i,'*') == 2 for i in G.nodes())
+
+#First Iteration:
+m.optimize()
+FG = saveresult(draw=True)
+tour = findsubtour(FG, 'city 1')
+
+n=1
+
+#Can run each iteration separately to see how the graph changes with each added cut constraint
+while len(tour) != len(G.nodes()):
+    addcuttomodel(G, tour, m)
+    m.optimize()
+    n+=1
+    FG = saveresult(draw=False)
+    tour = findsubtour(FG, 'city 1')
+FG=saveresult(draw=True)
