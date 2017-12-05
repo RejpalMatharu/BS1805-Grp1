c4b014463d2c517e84267a66e402741d1dbe5d6f
yiting-wang <32194559+yiting-wang@users.noreply.github.com>
2017-12-05 20:14:20 +0000

    finish 2a, 2b, 2c, 2d

--- HW2_2_Yiting Wang.py
+++ HW2_2_Yiting Wang.py
@@ -56,11 +56,11 @@ print(dist_matrix)
 lons = data['longitude'].tolist()
 lats = data['latitude'].tolist()
 plt.figure(figsize=(18,18))
-plt.scatter(lons,lats)
+plt.scatter(lons, lats)
 
 
 # Question 2b
-# reference from https://github.com/google/or-tools/blob/master/examples/python/tsp.py
+# https://github.com/google/or-tools/blob/master/examples/python/tsp.py
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
 
@@ -82,12 +82,127 @@ if assignment:
     # Inspect solution.
     # Only one route here; otherwise iterate from 0 to routing.vehicles() - 1
     route_number = 0
+    rount_list = []
     node = routing.Start(route_number)
     route = ''
     while not routing.IsEnd(node):
         route += 'city ' + str(node + 1) + ' -> '
+        rount_list.append(node)
         node = assignment.Value(routing.NextVar(node))
     route += 'city 1'
     print(route)
 else:
     print('No solution found.')
+
+# Question 2d for 2b
+
+x1 = []
+y1 = []
+
+for i in route_list:
+    x1.append(lons[i])
+    y1.append(lats[i])
+
+plt.plot(x1, y1, "b--", linewidth=1)
+plt.show()
+
+# Question 2c
+# http://examples.gurobi.com/traveling-salesman-problem/
+from gurobipy import *
+
+# Callback - use lazy constraints to eliminate sub-tours
+
+def subtourelim(model, where):
+  if where == GRB.callback.MIPSOL:
+    selected = []
+    # make a list of edges selected in the solution
+    for i in range(n):
+      sol = model.cbGetSolution([model._vars[i,j] for j in range(n)])
+      selected += [(i,j) for j in range(n) if sol[j] > 0.5]
+    # find the shortest cycle in the selected edge list
+    tour = subtour(selected)
+    if len(tour) < n:
+      # add a subtour elimination constraint
+      expr = 0
+      for i in range(len(tour)):
+        for j in range(i+1, len(tour)):
+          expr += model._vars[tour[i], tour[j]]
+      model.cbLazy(expr <= len(tour)-1)
+
+
+# Given a list of edges, finds the shortest subtour
+
+def subtour(edges):
+  visited = [False]*n
+  cycles = []
+  lengths = []
+  selected = [[] for i in range(n)]
+  for x,y in edges:
+    selected[x].append(y)
+  while True:
+    current = visited.index(False)
+    thiscycle = [current]
+    while True:
+      visited[current] = True
+      neighbors = [x for x in selected[current] if not visited[x]]
+      if len(neighbors) == 0:
+        break
+      current = neighbors[0]
+      thiscycle.append(current)
+    cycles.append(thiscycle)
+    lengths.append(len(thiscycle))
+    if sum(lengths) == n:
+      break
+  return cycles[lengths.index(min(lengths))]
+
+n = 38
+
+m = Model()
+
+# Create variables
+
+vars = {}
+for i in range(n):
+   for j in range(i+1):
+     vars[i,j] = m.addVar(obj= dist_matrix[i][j], vtype=GRB.BINARY,
+                          name='e'+str(i)+'_'+str(j))
+     vars[j,i] = vars[i,j]
+   m.update()
+
+# Add degree-2 constraint, and forbid loops
+
+for i in range(n):
+  m.addConstr(quicksum(vars[i,j] for j in range(n)) == 2)
+  vars[i,i].ub = 0
+
+m.update()
+
+# Optimize model
+
+m._vars = vars
+m.params.LazyConstraints = 1
+m.optimize(subtourelim)
+
+solution = m.getAttr('x', vars)
+selected = [(i,j) for i in range(n) for j in range(n) if solution[i,j] > 0.5]
+assert len(subtour(selected)) == n
+
+new_route_list = subtour(selected)
+new_route_list.append(0)
+
+new_route = ""
+for i in new_route_list:
+    new_route = new_route + "city" + str(i + 1) + ' -> '
+   
+print(new_route[:-4])
+
+# Question 2d for 2b
+x2 = []
+y2 = []
+
+for i in new_route_list:
+    x2.append(lons[i])
+    y2.append(lats[i])
+
+plt.plot(x2, y2, "b--", linewidth=1)
+plt.show()
